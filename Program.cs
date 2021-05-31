17d3c3b3f20ebe80bad796f435c12dc2e54e90c9
Denis Ronzhin <denisronzhin@yandex.ru>
2021-05-31 15:16:02 +1000

    test

--- Program.cs
+++ Program.cs
@@ -407,53 +407,7 @@ namespace PR_4
     }
 
 
-    public class Apple:IDisposable
-    {
-        public struct appPointer
-        {public int x,y;}
-
-        public appPointer appPointer_ {get; set;} //свойство с координатами яблока
-
-        appPointer appPoint = new appPointer();
-
-        //Метод для создания яблока на поле
-        public void createApple()
-        {
-           Random randomX = new Random();
-           Random randomY = new Random();
-
-           appPoint.x = randomX.Next(4,SetupWindowSize.wight-4);
-           appPoint.y = randomY.Next(4,SetupWindowSize.height-4);
-
-           CurrentValue.apple_x = appPoint.x;
-           CurrentValue.apple_y = appPoint.y;
-   
-        }
-
-        //Метод для отрисовки яблока на поле
-        public void showApple()
-        {   
- 
-           Console.SetCursorPosition(appPoint.x,appPoint.y);
-           Console.Write('X');    
     
-  
-        }    
- 
-        public void hideApple()
-        {   
- 
-           Console.SetCursorPosition(appPoint.x,appPoint.y);
-           Console.Write(' ');    
-    
-        //хуй
-        //хуй        
-        }
-
-        public void Dispose()
-        {
-        }    
-    }
 
     class Program
     {
